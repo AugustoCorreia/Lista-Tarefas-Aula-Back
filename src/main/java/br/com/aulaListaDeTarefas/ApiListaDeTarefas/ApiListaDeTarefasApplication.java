a656def96c61648dffa090ef7c44b039b28f4532
Augusto Correia Lira <augustocorreia@frwk.com.br>
2021-10-25 22:56:13 -0300

    initial commit

---
+++ src/main/java/br/com/aulaListaDeTarefas/ApiListaDeTarefas/ApiListaDeTarefasApplication.java
@@ -0,0 +1,13 @@
+package br.com.aulaListaDeTarefas.ApiListaDeTarefas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiListaDeTarefasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiListaDeTarefasApplication.class, args);
+	}
+
+}
